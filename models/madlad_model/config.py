5b549ef371c00028634010463a0f8f25aa315c98
paulvinueza30 <paulvinueza30@gmail.com>
2025-03-06 20:48:52 -0500

    fixed att_imp

--- models/madlad_model/config.py
+++ models/madlad_model/config.py
@@ -2,6 +2,6 @@ import torch
 
 MODEL_NAME = "jbochi/madlad400-7b-mt"
 QUANTIZATION_CONFIG = None  # Adjust if using quantization
-ATTN_IMPLEMENTATION = "default"  # Modify if needed
+ATTN_IMPLEMENTATION = "eager"  # Modify if needed
 DEVICE_MAP = "auto"  # Change based on GPU/CPU setup
 TORCH_DTYPE = torch.float16
