c272f7038e2c7fffe46457036e57f64b622708b3
paulvinueza30 <paulvinueza30@gmail.com>
2025-03-06 20:47:54 -0500

    fixed torch type in madlad config

--- models/madlad_model/config.py
+++ models/madlad_model/config.py
@@ -1,5 +1,7 @@
+import torch
+
 MODEL_NAME = "jbochi/madlad400-7b-mt"
 QUANTIZATION_CONFIG = None  # Adjust if using quantization
 ATTN_IMPLEMENTATION = "default"  # Modify if needed
 DEVICE_MAP = "auto"  # Change based on GPU/CPU setup
-TORCH_DTYPE = "torch.float16"  # Adjust based on precision requirements
+TORCH_DTYPE = torch.float16
