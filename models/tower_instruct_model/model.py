b03aa8b227d66bfca3a1dc57dc6570ec09ff0105
paulvinueza30 <paulvinueza30@gmail.com>
2025-03-10 03:37:40 -0400

    changed instruct model to use gpu

--- models/tower_instruct_model/model.py
+++ models/tower_instruct_model/model.py
@@ -1,14 +1,16 @@
+import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from .config import MODEL_NAME
 
 def towerinstruct_load():
     """
-    Load the TowerInstruct model and tokenizer.
+    Load the TowerInstruct model and tokenizer on GPU if available.
     """
     try:
+        device = "cuda" if torch.cuda.is_available() else "cpu"
         tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-        model = AutoModelForCausalLM.from_pretrained(MODEL_NAME)
-        return model, tokenizer
+        model = AutoModelForCausalLM.from_pretrained(MODEL_NAME).to(device)
+        return model, tokenizer, device
     except Exception as e:
         print(f"❌ Error loading {MODEL_NAME}: {e}")
-        return None, None
+        return None, None, None
