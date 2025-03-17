8de27186c356e0bced17b1e304a06e28bdb5e33d
paulvinueza30 <paulvinueza30@gmail.com>
2025-03-16 20:55:21 -0400

    delete everything in cashe auto

--- scripts/runs_tests.sh
+++ scripts/runs_tests.sh
@@ -1,27 +1,33 @@
-# Make it executable:
-# chmod +x scripts/run_tests.sh
-# run it:
-# ./scripts/run_tests.sh
 #!/bin/bash
 echo "Running translation tests..."
-pytest -v -s tests/test_europarl_small100.py
-huggingface-cli delete-cache
-pytest -v -s tests/test_tedTalk_small100.py
-huggingface-cli delete-cache
-pytest -v -s tests/test_wmt_small100.py
-huggingface-cli delete-cache
-python3 -m tests.test_Helsinki.py
-huggingface-cli delete-cache
-pytest -v -s tests/test_M2M.py
-huggingface-cli delete-cache
-pytest -v -s tests/test_NLLB.py
-huggingface-cli delete-cache
-python3 -m tests.test_towerinstruct.py
-huggingface-cli delete-cache
-python3 -m tests.test_googletranslate.py
-huggingface-cli delete-cache
-python3 -m tests.test_gemini.py
-huggingface-cli delete-cache
-python3 -m tests.test_chatGPT.py
-huggingface-cli delete-cache
 
+# Run tests
+pytest -v -s tests/test_europarl_small100.py
+rm -rf ~/.cache/huggingface/hub  # Delete Hugging Face cache
+
+pytest -v -s tests/test_tedTalk_small100.py
+rm -rf ~/.cache/huggingface/hub  # Delete Hugging Face cache
+
+pytest -v -s tests/test_wmt_small100.py
+rm -rf ~/.cache/huggingface/hub  # Delete Hugging Face cache
+
+python3 -m tests.test_Helsinki.py
+rm -rf ~/.cache/huggingface/hub  # Delete Hugging Face cache
+
+pytest -v -s tests/test_M2M.py
+rm -rf ~/.cache/huggingface/hub  # Delete Hugging Face cache
+
+pytest -v -s tests/test_NLLB.py
+rm -rf ~/.cache/huggingface/hub  # Delete Hugging Face cache
+
+python3 -m tests.test_towerinstruct.py
+rm -rf ~/.cache/huggingface/hub  # Delete Hugging Face cache
+
+python3 -m tests.test_googletranslate.py
+rm -rf ~/.cache/huggingface/hub  # Delete Hugging Face cache
+
+python3 -m tests.test_gemini.py
+rm -rf ~/.cache/huggingface/hub  # Delete Hugging Face cache
+
+python3 -m tests.test_chatGPT.py
+rm -rf ~/.cache/huggingface/hub  # Delete Hugging Face cache
