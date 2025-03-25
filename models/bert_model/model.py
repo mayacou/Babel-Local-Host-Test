396bc934e10fdf926b47ea5ccb06d8c7c53eaf21
paulvinueza30 <paulvinueza30@gmail.com>
2025-03-25 19:03:03 -0400

    changed madlad to bert

--- models/bert_model/model.py
+++ models/bert_model/model.py
@@ -1,8 +1,8 @@
-from transformers import MBartForConditionalGeneration, MBartTokenizer
+from transformers import MBartForConditionalGeneration, MBart50Tokenizer
 
 def bert_load(model_name="facebook/mbart-large-50-many-to-many-mmt"):
     # Load the mBART tokenizer and model
-    tokenizer = MBartTokenizer.from_pretrained(model_name)
+    tokenizer = MBart50Tokenizer.from_pretrained(model_name)
     model = MBartForConditionalGeneration.from_pretrained(model_name)
 
     return model, tokenizer
