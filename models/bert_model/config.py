4b5a10c230ab4195dcc59f15c0f9748a981bffb4
paulvinueza30 <paulvinueza30@gmail.com>
2025-03-25 18:34:37 -0400

    changed madlad to bert

--- models/bert_model/config.py
+++ models/bert_model/config.py
@@ -1,6 +1,6 @@
 # config.py
-MODEL_NAME = 'bert-base-multilingual-cased'  # Replace with your desired model for multi-language tasks
-TOKENIZER_NAME = 'bert-base-multilingual-cased'  # Ensure tokenizer matches the model
+MODEL_NAME = 'google-bert/bert-base-uncased'  # Replace with your desired model for multi-language tasks
+TOKENIZER_NAME = MODEL_NAME  # Ensure tokenizer matches the model
 DEVICE_MAP = 'auto'  # Change based on your hardware setup (e.g., 'cuda', 'cpu', 'auto')
 TORCH_DTYPE = 'float32'  # Data type for model weights
 PAD_TOKEN = '[PAD]'  # The padding token, you can customize this if needed
